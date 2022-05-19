80818c3e2f34f4696766bdc198d91b830ef33b21
Anderson <100314954+Anders1718@users.noreply.github.com>
2022-05-19 16:50:51 -0500

    Borrar conexion

--- src/ventanas1/BuscarClientePorsihayError.java
+++ src/ventanas1/BuscarClientePorsihayError.java
@@ -23,9 +23,7 @@ import javax.swing.table.DefaultTableModel;
  * @author User
  */
 public class BuscarClientePorsihayError extends javax.swing.JFrame {
-    String user = "sql5454836";
-    String clave = "pkzsvhf4Qx";
-    String url = "jdbc:mysql://sql5.freemysqlhosting.net/sql5454836";
+   
     Connection con ;
     Statement stmt;
     ResultSet rs;
