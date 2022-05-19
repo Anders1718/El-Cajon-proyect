00ae58e7ea6e63f2b4a87d14c80a249223311f90
Anderson <100314954+Anders1718@users.noreply.github.com>
2022-05-19 16:54:59 -0500

    Borrar Conexion

--- src/ventanas1/Gastos.java
+++ src/ventanas1/Gastos.java
@@ -29,9 +29,7 @@ import static ventanas1.FacturaPanel.jTextFieldCorreo;
  * @author User
  */
 public class Gastos extends javax.swing.JFrame {
-    String user = "sql5454836";
-    String clave = "pkzsvhf4Qx";
-    String url = "jdbc:mysql://sql5.freemysqlhosting.net/sql5454836";
+   
     Connection con;
     Statement stmt;
     ResultSet rs;
